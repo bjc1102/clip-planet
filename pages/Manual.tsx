936e27c80ca9765c01fcd157b65b8712ad79c884
byung ju choi <bjc30849@gmail.com>
2022-06-26 17:10:40 +0900

    fix: ban-ts-ignore off

--- pages/Manual.tsx
+++ pages/Manual.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { Octokit } from '@octokit/core'
 
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 
@@ -12,11 +13,26 @@ const Manual: NextPage = ({
 export default Manual
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios({
-    url: 'https://api.github.com/repos/zafar-saleem/anymnd/git/blobs/7af90ceab018a889b46634331cab822d94ff2b19',
+  // const response = await axios({
+  //   url: 'https://api.github.com/repos/zafar-saleem/anymnd/git/blobs/7af90ceab018a889b46634331cab822d94ff2b19',
+  // })
+  // console.log('response: ', response.data)
+  const token = process.env.ACCESS_TOKEN
+  const octokit = new Octokit({
+    auth: token,
   })
-  console.log('response: ', response.data)
-  const text = await response.data.content
+  const result = await octokit.request(
+    'GET /repos/{owner}/{repo}/contents/{path}',
+    {
+      owner: 'bjc1102',
+      repo: 'startup-page',
+      path: 'README.md',
+    }
+  )
+  // const decodeData = atob(result.data)
+  //@ts-ignore
+  console.log(result.data.content)
+  const text = '123'
   return {
     props: {
       text,
