8f443e0ea0b07538bebc9b80bc05220ac0ad67bb
bjc1102 <bjc30849@gmail.com>
2022-11-16 22:29:05 +0900

    fix: 컴포넌트 초기화

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,14 @@
+import GoogleIcon from 'public/assets/GoogleIcon'
 import React from 'react'
 
 const Nav: React.FC = () => {
   return (
     <nav className="fixed py-4 w-full border-solid border-b border-black">
-      <div className="flex max-w-6xl mx-auto justify-between">
-        <span>Bootingsi</span>
-        <span>Login</span>
+      <div className="flex max-w-5xl mx-auto justify-between items-center">
+        <span>Logo</span>
+        <span className="hover:bg-slate-200 rounded-full p-1">
+          Notification
+        </span>
       </div>
     </nav>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import { NextPage } from 'next'
 import React from 'react'
+import Login from '@/components/Login'
 
 //BEM 방식 ( block , element , model )
 
 const Home: NextPage = () => {
-  return <main className="max-w-5xl mx-auto px-16"></main>
+  return (
+    <main className="max-w-5xl min-h-screen mx-auto px-16">
+      <Login />
+    </main>
+  )
 }
 
 export default Home
