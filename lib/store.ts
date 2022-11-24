e514eacb2eb40e411cc6041ee42d287812fd4752
bjc1102 <bjc30849@gmail.com>
2022-11-24 21:56:31 +0900

    feat: zustand 추가, recoil 삭제

---
+++ lib/store.ts
@@ -0,0 +1,23 @@
+import create, { StateCreator } from 'zustand'
+import { persist, PersistOptions } from 'zustand/middleware'
+
+interface Token {
+  refreshToken: string
+}
+
+type MyPersist = (
+  config: StateCreator<Token>,
+  options: PersistOptions<Token>
+) => StateCreator<Token>
+
+export const useTokenStore = create<Token>(
+  (persist as MyPersist)(
+    (set, get) => ({
+      refreshToken: localStorage.getItem('refresh-token') || '',
+    }),
+    {
+      name: 'refresh-token', // name of item in the storage (must be unique)
+      getStorage: () => localStorage, // (optional) by default the 'localStorage' is used
+    }
+  )
+)
