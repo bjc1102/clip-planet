92c73f351e3a118be8f399aa75c6b2341cc08666
bjc1102 <bjc30849@gmail.com>
2022-12-18 21:46:16 +0900

    chore: 자식요소 위치 변경

--- components/MainContent/Card.tsx
+++ components/MainContent/Card.tsx
@@ -14,7 +14,7 @@ const Card = () => {
       className="group max-w-[280px] overflow-hidden hover:cursor-pointer"
     >
       <div className="group-hover:shadow-[inset_0_-55px_75px_-55px_rgba(0,0,0,1)] relative py-20 rounded-lg bg-[url('https://avatars.githubusercontent.com/u/71929440?v=4?s=400')] bg-no-repeat bg-center bg-cover">
-        <div className="absolute group-hover:text-white bottom-2 px-2">
+        <div className="absolute group-hover:text-white bottom-0 right-0 px-2 py-2">
           <CardMenuBar />
         </div>
       </div>
@@ -23,7 +23,7 @@ const Card = () => {
         <img
           src="https://www.techinterviewhandbook.org/img/favicon.png"
           alt="site-favicon"
-          className="w-5 h-5 bg-white bg-opacity-0 object-cover rounded-md overflow-clip"
+          className="w-6 h-6 bg-white bg-opacity-0 object-cover rounded-md overflow-clip"
         />
         <h5 className="text-base font-semibold tracking-tight text-accentColor2 group-hover:text-gray-500">
           {sliceString('Noteworthy technology acquisitions 2021', 26)}
