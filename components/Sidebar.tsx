6df0195e41dda8ccee3484e676f69c6ef0a7e329
bjc1102 <bjc30849@gmail.com>
2022-12-21 11:13:31 +0900

    feat: Icon 추가, 변경

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Icon from 'public/assets/Icon'
-import useWindowSize from '@/hooks/useWindowSize'
 
 const Sidebar = () => {
   // const { windowSize } = useWindowSize()
@@ -10,7 +9,7 @@ const Sidebar = () => {
       <div className="min-h-full fixed w-60 overflow-y-auto py-4 px-3 bg-primaryColor2">
         <a
           href="https://flowbite.com/"
-          className="flex gap-2 items-center pl-2.5 mb-5"
+          className="flex [&>svg]:fill-accentColor1 gap-2 items-center pl-2.5 mb-5"
         >
           <Icon />
           <span className="self-center text-white text-xl font-semibold whitespace-nowrap">
@@ -19,66 +18,15 @@ const Sidebar = () => {
         </a>
         <ul className="space-y-2">
           <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="ml-3">Dashboard</span>
-            </a>
+            <div className="flex items-center cursor-pointer p-2 text-base font-normal text-white rounded-lg hover:bg-gray-700">
+              <span className="ml-3">대쉬보드</span>
+            </div>
           </li>
           <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-              <span className="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                Pro
-              </span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-              <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
-                3
-              </span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-            </a>
+            <div className="flex group [&>svg]:fill-accentColor2 items-center cursor-pointer p-2 text-base font-normal text-white rounded-lg hover:bg-gray-700">
+              <Icon />
+              <span className="ml-3">클립 추가하기</span>
+            </div>
           </li>
         </ul>
       </div>
--- public/assets/Directory.tsx
+++ public/assets/Directory.tsx
@@ -3,17 +3,11 @@ import React from 'react'
 function Directory() {
   return (
     <svg
-      aria-hidden="true"
-      className="w-6 h-6"
-      fill="currentColor"
-      viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 576 512"
+      className="w-6 h-6 fill-accentColor2"
     >
-      <path
-        clipRule="evenodd"
-        d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 00-2 0v1.586l-.293-.293a.999.999 0 10-1.414 1.414l2 2a.999.999 0 001.414 0l2-2a.999.999 0 10-1.414-1.414l-.293.293V9z"
-        fillRule="evenodd"
-      ></path>
+      <path d="M572.6 270.3l-96 192C471.2 473.2 460.1 480 447.1 480H64c-35.35 0-64-28.66-64-64V96c0-35.34 28.65-64 64-64h117.5c16.97 0 33.25 6.742 45.26 18.75L275.9 96H416c35.35 0 64 28.66 64 64v32h-48V160c0-8.824-7.178-16-16-16H256L192.8 84.69C189.8 81.66 185.8 80 181.5 80H64C55.18 80 48 87.18 48 96v288l71.16-142.3C124.6 230.8 135.7 224 147.8 224h396.2C567.7 224 583.2 249 572.6 270.3z" />
     </svg>
   )
 }
