71fac4d30c98ebee858c80fce006e1d61f02d614
bjc1102 <bjc30849@gmail.com>
2023-01-18 10:23:02 +0900

    feat: 받은 데이터 뿌려주기

--- components/Card/ClipCard.tsx
+++ components/Card/ClipCard.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import sliceString from '@/utils/sliceString'
 import CardMenuBar from '@/components/Card/CardMenuBar'
 import { AnimatePresence } from 'framer-motion'
+import { ClipType } from '@/types/clip'
 
-const ClipCard = () => {
-  const openInNewTab = (url?: string) => () => {
-    window.open('https://github.com/bjc1102', '_blank')?.focus
-  }
+interface ClipCardProps {
+  clip: ClipType
+}
+
+const ClipCard = ({ clip }: ClipCardProps) => {
   const hoverRef = React.useRef<HTMLDivElement>(null)
   const [isHover, setIsHover] = React.useState(false)
   const hoverMouseEvent = (nowHover: boolean) => {
@@ -15,6 +17,9 @@ const ClipCard = () => {
     else return () => setIsHover(false)
   }
 
+  const openInNewTab = () => () => {
+    window.open(clip.ogUrl, '_blank')?.focus
+  }
   return (
     <div
       onClick={openInNewTab()}
@@ -23,7 +28,10 @@ const ClipCard = () => {
       onMouseLeave={hoverMouseEvent(false)}
       className="group w-full lg:max-w-7xl overflow-hidden hover:cursor-pointer"
     >
-      <div className="group-hover:shadow-[inset_0_-55px_75px_-55px_rgba(0,0,0,1)] relative py-20 rounded-lg bg-[url('https://avatars.githubusercontent.com/u/71929440?v=4?s=400')] bg-no-repeat bg-center bg-cover">
+      <div
+        style={{ backgroundImage: `url(${clip.ogImage})` }}
+        className="group-hover:shadow-[inset_0_-55px_75px_-55px_rgba(0,0,0,1)] relative py-20 rounded-lg bg-no-repeat bg-center bg-cover"
+      >
         <div className="absolute group-hover:text-white bottom-0 group-hover:transition-opacity duration-500 right-0 px-2 py-2">
           <AnimatePresence>{isHover && <CardMenuBar />}</AnimatePresence>
         </div>
@@ -31,16 +39,16 @@ const ClipCard = () => {
       <div className="flex gap-2 items-center pt-3">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://www.techinterviewhandbook.org/img/favicon.png"
+          src={clip.favicon}
           alt="site-image"
-          className="w-5 h-5 bg-white bg-opacity-0 object-cover rounded-md overflow-clip"
+          className="w-5 h-5 bg-white object-cover rounded-full overflow-clip"
         />
-        <h5 className="text-lg font-semibold tracking-tight text-accentColor2 group-hover:text-gray-500">
-          {sliceString('github', 30)}
+        <h5 className="text-base font-semibold tracking-tight text-accentColor2 group-hover:text-gray-500">
+          {sliceString(clip.ogTitle, 24)}
         </h5>
       </div>
-      <span className="text-gray-500 group-hover:text-gray-700">
-        {sliceString('Noteworthy technology acquisitions 2021', 150)}
+      <span className="text-gray-500 group-hover:text-gray-700 text-sm">
+        {sliceString(clip.ogDescription, 45)}
       </span>
     </div>
   )
--- components/Card/ClipCardList.tsx
+++ components/Card/ClipCardList.tsx
@@ -7,16 +7,12 @@ const ClipCardList = () => {
 
   const clipSpreader = function () {
     return clipList?.map((clip) => {
-      return <ClipCard key={clip.id} />
+      return <ClipCard clip={clip} key={clip.id} />
     })
   }
 
   return (
     <section className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center">
-      <ClipCard />
-      <ClipCard />
-      <ClipCard />
-      <ClipCard />
       {clipSpreader()}
     </section>
   )
