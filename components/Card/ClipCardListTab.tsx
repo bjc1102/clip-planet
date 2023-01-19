03891f3d9fe0cb649633e0ac7a83a3d6fb53b624
bjc1102 <bjc30849@gmail.com>
2023-01-19 10:21:49 +0900

    feat: input 커스텀, useForm을 통한 validation

--- components/Card/ClipCardListTab.tsx
+++ components/Card/ClipCardListTab.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import useForm from '@/hooks/useForm'
+import Input from '@/components/common/Input'
+import useSetClip from '@/components/Card/queries/useSetClip'
+import { URLType } from '@/types/clip'
+import urlValidation from '@/utils/validate'
+import { siteURL } from 'static/const'
 
 const ClipCardListTab = () => {
+  const { setClip } = useSetClip()
+  const { error, handleChange, handleSubmit } = useForm<URLType>({
+    initialValue: { siteURL: '' },
+    validate: urlValidation,
+    onSubmit: ({ siteURL }) => setClip(siteURL),
+  })
+
+  console.log('message', error)
+
   return (
     <div className="my-10">
       <h3 className="inline-block bg-gray-500 p-1.5 rounded-md">
         ClipCardListTab
       </h3>
+      <form className="bg-white" onSubmit={handleSubmit}>
+        <Input
+          label="add-clip"
+          type="url"
+          onChange={handleChange}
+          name={siteURL}
+        />
+      </form>
     </div>
   )
 }
---
+++ static/const.ts
@@ -0,0 +1,3 @@
+export const token = 'access-token' as const
+export const refreshToken = 'refresh-token' as const
+export const siteURL = 'siteURL'
