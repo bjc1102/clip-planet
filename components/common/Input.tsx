0411baea1ea3355b328dc4b62a513ff68367942d
bjc1102 <bjc30849@gmail.com>
2023-01-20 10:25:53 +0900

    chore: 반응형 수정 및 자잘한 css 수정

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -6,7 +6,7 @@ const Input = (props: InputProps) => {
   return (
     <input
       {...props}
-      className="block w-full p-3 pl-5 text-sm border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+      className="block w-full p-3 pl-5 pr-20 text-sm border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
     />
   )
 }
