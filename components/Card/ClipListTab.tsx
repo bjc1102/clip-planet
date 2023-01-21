9a44250f11751b1446bdd927ec7eac6b5ebacc64
bjc1102 <bjc30849@gmail.com>
2023-01-21 21:45:53 +0900

    test: 로그인 여부  테스트 중

--- components/Card/ClipListTab.tsx
+++ components/Card/ClipListTab.tsx
@@ -1,18 +1,7 @@
 import React from 'react'
-import { useQueryClient } from '@tanstack/react-query'
-import { UserClipListKey } from 'static/query.key'
-import { ClipType } from '@/types/clip'
-import useGetClipList from './queries/useGetClipList'
 
 const ClipListTab = () => {
-  const { data: clipList } = useGetClipList()
-
-  return (
-    <div className="my-16">
-      CLIP LiST 입니다
-      <span>{clipList?.length}</span>
-    </div>
-  )
+  return <div className="my-16">CLIP LiST 입니다</div>
 }
 
 export default ClipListTab
---
+++ components/Layout/LoginLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginButton from '../Login/LoginButton'
+
+const LoginLayout = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <LoginButton />
+    </div>
+  )
+}
+
+export default LoginLayout
