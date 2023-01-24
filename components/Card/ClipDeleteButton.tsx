ae2545c8a061db16d945f7d912e3da5cffa53bae
bjc1102 <bjc30849@gmail.com>
2023-01-24 20:46:01 +0900

    feat: confirm modal 연결

--- components/Card/ClipDeleteButton.tsx
+++ components/Card/ClipDeleteButton.tsx
@@ -1,29 +1,53 @@
+import useModal from '@/hooks/useModal'
 import { useQueryClient } from '@tanstack/react-query'
 import { UserClipListKey } from 'constant/query.key'
 import CloseIcon from 'public/assets/CloseIcon'
 import LoadingIcon from 'public/assets/LoadingIcon'
 import React from 'react'
+import ConfirmModal from '../common/ConfirmModal'
 import useDeleteClip from './queries/useDeleteClip'
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
+interface ClipDeleteButtonProps {
+  id: number
+}
 
-const ClipDeleteButton = (props: Props) => {
+const ClipDeleteButton = ({ id }: ClipDeleteButtonProps) => {
+  const { isModalOpen, handleModalClose, handleModalOpen } = useModal()
   const { mutate: deleteClip, isLoading } = useDeleteClip()
   const queryClient = useQueryClient()
 
-  const handleClick = function (e: React.MouseEvent<HTMLButtonElement>) {
+  const handleDeleteButtonClick = function (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) {
     e.stopPropagation()
-    queryClient.invalidateQueries(UserClipListKey)
+    handleModalOpen()
+  }
+
+  const handleDeleteClip = function () {
+    deleteClip(id, {
+      onSuccess() {
+        queryClient.invalidateQueries(UserClipListKey)
+      },
+    })
   }
 
   return (
-    <button
-      className="p-1 rounded-full border border-primaryColor1 hover:text-gray-400 hover:border-accentColor1 text-white bg-primaryColor2 focus:outline-none"
-      onClick={handleClick}
-      disabled={isLoading}
-    >
-      {isLoading ? <LoadingIcon /> : <CloseIcon />}
-    </button>
+    <>
+      <button
+        className="p-1 rounded-full border border-primaryColor1 hover:text-gray-400 hover:border-accentColor1 text-white bg-primaryColor2 focus:outline-none"
+        onClick={handleDeleteButtonClick}
+        disabled={isLoading}
+      >
+        {isLoading ? <LoadingIcon /> : <CloseIcon />}
+      </button>
+      {isModalOpen && (
+        <ConfirmModal
+          content="정말로 삭제하시겠습니까?"
+          closeCallback={handleModalClose}
+          submitCallback={handleDeleteClip}
+        />
+      )}
+    </>
   )
 }
 
