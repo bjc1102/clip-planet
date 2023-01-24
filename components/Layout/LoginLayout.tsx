44b054ca248533940f75872ca27d1b4941e0a562
bjc1102 <bjc30849@gmail.com>
2023-01-24 10:55:22 +0900

    feat: login layout css, content 추가

--- components/Layout/LoginLayout.tsx
+++ components/Layout/LoginLayout.tsx
@@ -1,10 +1,21 @@
+import PlanetIcon from 'public/assets/PlanetIcon'
 import React from 'react'
 import LoginButton from '../Login/LoginButton'
 
 const LoginLayout = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <LoginButton />
+    <div className="p-96">
+      <div className="absoluteCenter flex flex-col justify-center items-center">
+        <div className="w-12 h-12 [&>*]:fill-accentColor1">
+          <PlanetIcon />
+        </div>
+        <strong className="block text-xl my-4 text-white">
+          로그인이 필요한 서비스입니다.
+        </strong>
+        <div className="[&>*]:text-base">
+          <LoginButton />
+        </div>
+      </div>
     </div>
   )
 }
