fcf2276558109ccf463c2d2688c4796548719163
bjc1102 <bjc30849@gmail.com>
2023-01-30 10:14:56 +0900

    test: 대소문자 테스트

--- src/utils/getExpires.ts
+++
@@ -1,8 +0,0 @@
-const expire = (token: 'refresh-token' | 'token') => {
-  const twoWeek = 14 * 24 * 3600 * 1000; //1 weeks
-  const twoHour = 2 * (60 * 60 * 1000);
-  if (token === 'refresh-token') return new Date(Date.now() + twoWeek);
-  else return new Date(Date.now() + twoHour);
-};
-
-export default expire;
