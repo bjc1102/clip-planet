9c4c118bdbf1a22f5a25f5d5b5b4330993c07695
bjc1102 <bjc30849@gmail.com>
2023-03-18 15:32:16 +0900

    feat: 공간 차지하는 nav item 삭제

--- components/Header/MenuBar.tsx
+++ components/Header/MenuBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import BookIcon from 'public/assets/BookIcon'
 
 const MenuBar = () => {
   return (
@@ -10,15 +9,6 @@ const MenuBar = () => {
       >
         시작하기
       </a>
-      <button
-        data-collapse-toggle="mobile-menu-2"
-        type="button"
-        className="inline-flex items-center p-2 ml-1 text-sm rounded-lg lg:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
-        aria-controls="mobile-menu-2"
-        aria-expanded="false"
-      >
-        <BookIcon />
-      </button>
     </div>
   )
 }
